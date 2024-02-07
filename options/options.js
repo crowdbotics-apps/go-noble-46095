895e4b686c464aa4f4d9d46407f9a93877f92d08
Crowdbotics <team@crowdbotics.com>
2024-02-07 18:18:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "go-noble-46095",
+  url: "https://go-noble-46095.botics.co",
+  api: "https://go-noble-46095.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
